832992471b1de6dd8206030b1fe2caedcd8db374
Niclas Olofsson <n@niclasolofsson.se>
2016-11-30 08:50:14 +0100

    Add option to open non-existing files.

--- open_related.py
+++ open_related.py
@@ -2,12 +2,20 @@ import sublime, sublime_plugin
 import os.path
 from . import converter
 
+
+def _open_file(window, filename):
+    if window.num_groups() > 1:
+        window.focus_group((window.active_group() + 1) % window.num_groups())
+
+    window.open_file(filename)
+
+
 class OpenRelatedCommand(sublime_plugin.WindowCommand):
     def run(self):
-        win = self.window
-        view = win.active_view()
+        view = self.window.active_view()
         current_file = view.file_name()
         found = False
+        non_existing_files = []
 
         settings = view.settings()
         plugin_settings = settings.get('open_related', {})
@@ -15,13 +23,17 @@ class OpenRelatedCommand(sublime_plugin.WindowCommand):
         for patterns in plugin_settings.get('patterns', []):
             for file in converter.create(patterns, sublime.platform()).convert(current_file):
                 if os.path.exists(file):
-                    if win.num_groups() > 1:
-                        win.focus_group((win.active_group() + 1) % win.num_groups())
-                    self.window.open_file(file)
+                    _open_file(self.window, file)
                     found = True
-            if found: return
+                else:
+                    non_existing_files.append(file)
+            if found:
+                return
 
-        sublime.status_message("Cannot find related file !")
+        if non_existing_files and plugin_settings.get('create_new_if_not_exist', False):
+            _open_file(self.window, non_existing_files[0])
+
+        sublime.status_message("Cannot find related file!")
 
     def is_enabled(self):
         return self.window.active_view() != None
